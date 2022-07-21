45b765d13c1e8772c7871777ad38e4330a1b10c7
Michelle Chernick <michellechernick@Michelles-MacBook-Air.local>
2022-07-21 15:55:35 -0700

    header!

---
+++ astrochelle/utils/data_models/dm_propagator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# ------------------------------------------------------------------------------
+# dm_propagator
+# DESCRIPTION: Data models used in the propagator
+# AUTHOR: Michelle Chernick
+# CREATED: 2022-07-21
+# ------------------------------------------------------------------------------
+
