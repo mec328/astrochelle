9ef54eff8f4d219936aa0ee6d82d11710d72cb61
Michelle Chernick <michellechernick@Michelles-MacBook-Air.local>
2022-07-25 14:31:27 -0700

    that should do it on spacecraft for now

--- test/test_dm_spacecraft.py
+++ test/test_dm_spacecraft.py
@@ -8,8 +8,24 @@ import pytest
 from astrochelle.utils.data_models.dm_spacecraft import *
 
 def test_spacecraft_config_pass():
-    # TODO
-    pass
+    # Defaults
+    mass = 100
+    drag_coef = 1 
+    srp_coef = 1
+    drag_area = 1
+    srp_area = 1
+
+    # Nominal values should not raise exception
+    try:
+        spacecraft_config = SpacecraftConfig(
+            mass = mass,
+            coefficient_drag = drag_coef,
+            coefficient_srp = srp_coef,
+            effective_area_drag = drag_area,
+            effective_area_srp = srp_area
+            )
+    except Exception:
+        assert False
 
 def test_spacecraft_config_fail():
     # Defaults
