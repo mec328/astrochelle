24ba7f01d7b37c2d6133dbf0f808dda28c5320f9
Michelle Chernick <mcparakeet@gmail.com>
2022-07-26 17:42:10 -0700

    makin filez yo

---
+++ astrochelle/dynamics/absolute_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# ------------------------------------------------------------------------------
+# absolute_state
+# DESCRIPTION: absolute state structure and propagation classes
+# AUTHOR: Michelle Chernick
+# CREATED: 2022-07-25
+# ------------------------------------------------------------------------------
+
+# Python imports
+import numpy as np
+
+# Astrochelle imports
+from astrochelle.utils.data_models.dm_propagator import *
+
+###################
+# GVE Propagation #
+###################
+class GVEPropagator():
+    def __init__():
+        '''Gauss Variational Equation propagation class for 
+        Keplerian orbital elements
+
+        Args:
+
+        Attributes:
+            
+        '''
---
+++ astrochelle/dynamics/spacecraft.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# ------------------------------------------------------------------------------
+# spacecraft
+# DESCRIPTION: spacecraft class and methods!
+# AUTHOR: Michelle Chernick
+# CREATED: 2022-07-26
+# ------------------------------------------------------------------------------
+
+# Python imports
+import numpy as np
+
+# Astrochelle imports
+from astrochelle.utils.data_models.dm_spacecraft import *
+
+##############
+# Spacecraft #
+##############
+class Spacecraft():
+    def __init__():
+        '''Spacecraft class
+
+        Args:
+            TODO
+
+        Attributes:
+            TODO
+        '''
+
+        # 
