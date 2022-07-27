523db45ed9535679224f7a5a0be548a0844a4037
Michelle Chernick <mcparakeet@gmail.com>
2022-07-27 08:41:38 -0700

    unit tests, tho i feel like i need some checks on size of inputs

--- test/test_epoch.py
+++ test/test_epoch.py
@@ -8,7 +8,29 @@ import pytest
 from astrochelle.utils.epoch import *
 
 def test_epoch_initialization():
-    # TODO pass on ymdhms
+    # Pass on ymdhm (no seconds or nano seconds)
+    try:
+        epoch = Epoch(
+            year = 2022,
+            month = 7,
+            day = 27,
+            hours = 12,
+            minutes = 5)
+    except Exception:
+        assert False
+
+    # Pass on ymdhms(ns?)
+    try:
+        epoch = Epoch(
+            year = 2022,
+            month = 7,
+            day = 27,
+            hours = 12,
+            minutes = 5,
+            seconds = 5,
+            nanoseconds = 100)
+    except Exception:
+        assert False    
 
     # One or more of year, month, day, hours, minutes not provided on initialization 
     with pytest.raises(Exception):
