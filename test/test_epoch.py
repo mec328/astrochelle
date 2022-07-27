03439aec968011c2397786fa0a5b3c7e59090479
Michelle Chernick <mcparakeet@gmail.com>
2022-07-27 10:31:15 -0700

    o damn it's linted

--- test/test_epoch.py
+++ test/test_epoch.py
@@ -2,41 +2,43 @@
 # ------------------------------------------------------------------------------
 
 # Python imports
-import pytest 
+import pytest
 
 # Astrochelle imports
 from astrochelle.utils.epoch import *
+
 
 def test_epoch_initialization():
     # Pass on ymdhm (no seconds or nano seconds)
     try:
         epoch = Epoch(
-            year = 2022,
-            month = 7,
-            day = 27,
-            hours = 12,
-            minutes = 5)
+            year=2022,
+            month=7,
+            day=27,
+            hours=12,
+            minutes=5)
     except Exception:
         assert False
 
     # Pass on ymdhms(ns?)
     try:
         epoch = Epoch(
-            year = 2022,
-            month = 7,
-            day = 27,
-            hours = 12,
-            minutes = 5,
-            seconds = 5,
-            nanoseconds = 100)
+            year=2022,
+            month=7,
+            day=27,
+            hours=12,
+            minutes=5,
+            seconds=5,
+            nanoseconds=100)
     except Exception:
-        assert False    
+        assert False
 
-    # One or more of year, month, day, hours, minutes not provided on initialization 
+    # One or more of year, month, day, hours, minutes not provided on initialization
     with pytest.raises(Exception):
-        epoch = Epoch(year = 2022)
+        epoch = Epoch(year=2022)
 
     # TODO MORE HERE
+
 
 def test_check_validity_date():
     # Defaults
@@ -52,54 +54,69 @@ def test_check_validity_date():
 
     # Year not valid, should return False
     flag_valid, _ = check_validity_date(
-        year = -4800, month = month, day = day, hours = hours,
-        minutes = minutes, seconds = seconds, nanoseconds = nanoseconds
-        )
+        year=-4800, month=month, day=day, hours=hours,
+        minutes=minutes, seconds=seconds, nanoseconds=nanoseconds
+    )
 
     assert flag_valid == False
 
     # Month not valid, should return False
     flag_valid, _ = check_validity_date(
-        year = year, month = 13, day = day, hours = hours,
-        minutes = minutes, seconds = seconds, nanoseconds = nanoseconds
-        )
+        year=year, month=13, day=day, hours=hours,
+        minutes=minutes, seconds=seconds, nanoseconds=nanoseconds
+    )
 
     assert flag_valid == False
 
     # Day not in provided month, should return False
     flag_valid, _ = check_validity_date(
-        year = year, month = 13, day = day, hours = hours,
-        minutes = minutes, seconds = seconds, nanoseconds = nanoseconds
-        )
+        year=year, month=13, day=day, hours=hours,
+        minutes=minutes, seconds=seconds, nanoseconds=nanoseconds
+    )
 
     assert flag_valid == False
 
     # It's not a leap year but month is February and day is 29, should return False
     flag_valid, _ = check_validity_date(
-        year = 2021, month = 2, day = 29, hours = hours,
-        minutes = minutes, seconds = seconds, nanoseconds = nanoseconds
-        )
+        year=2021, month=2, day=29, hours=hours,
+        minutes=minutes, seconds=seconds, nanoseconds=nanoseconds
+    )
 
     assert flag_valid == False
 
     # It is a leap year and month is February and day is 29, should return True
     flag_valid, _ = check_validity_date(
-        year = 2020, month = 2, day = 29, hours = hours,
-        minutes = minutes, seconds = seconds, nanoseconds = nanoseconds
-        )
+        year=2020, month=2, day=29, hours=hours,
+        minutes=minutes, seconds=seconds, nanoseconds=nanoseconds
+    )
 
     assert flag_valid
 
+    # Negative hours
+    flag_valid, _ = check_validity_date(
+        year=year, month=month, day=day, hours=-10,
+        minutes=minutes, seconds=seconds, nanoseconds=nanoseconds
+    )
+
+    assert flag_valid == False
+
+    # Negative minutes
+    flag_valid, _ = check_validity_date(
+        year=year, month=month, day=day, hours=hours,
+        minutes=-5, seconds=seconds, nanoseconds=nanoseconds
+    )
+
+    assert flag_valid == False
+
     # MORE HERE TODO
+
 
 def test_check_leap_year():
     # Is leap year
-    assert check_leap_year(year = 2020)
+    assert check_leap_year(year=2020)
 
     # Is not divisible by 4, not leap year
-    assert not check_leap_year(year = 2021)
+    assert not check_leap_year(year=2021)
 
     # Is divisible by 4 but also by 100 and not by 400, not leap year
-    assert not check_leap_year(year = 1900)
-
-
+    assert not check_leap_year(year=1900)
