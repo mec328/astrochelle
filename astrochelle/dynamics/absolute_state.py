b3f93f35dfbfcbfd156b6210a8495da0fb8f5e17
Michelle Chernick <mcparakeet@gmail.com>
2022-08-03 17:28:24 -0700

    setting up frameworkkkk

--- astrochelle/dynamics/absolute_state.py
+++ astrochelle/dynamics/absolute_state.py
@@ -8,10 +8,27 @@
 
 # Python imports
 import numpy as np
+from copy import deepcopy
 
 # Astrochelle imports
 from astrochelle.utils.epoch import Epoch
 from astrochelle.utils.data_models.dm_propagator import GVEPropagatorConfig
+
+##################
+# Error Handling #
+##################
+
+
+class AbsoluteStateException(Exception):
+    '''Exceptions related to absolute state
+    '''
+
+    def __init__(
+        self,
+        msg: str = "Something went wrong in absolute_state.py."
+    ):
+
+        super().__init__(msg)
 
 ###################
 # GVE Propagation #
@@ -19,42 +36,95 @@ from astrochelle.utils.data_models.dm_propagator import GVEPropagatorConfig
 
 
 class GVEPropagator():
-    def __init__(self, propagator_config: GVEPropagatorConfig):
+    def __init__(self,
+                 propagator_config: GVEPropagatorConfig,
+                 initial_epoch: Epoch
+                 ):
         '''Gauss Variational Equation propagation class for 
         Keplerian orbital elements
 
         Args:
             propagator_config (`GVEPropagatorConfig`): propagator configuration
                 see `dm_propagator.py`
+            initial_epoch (`Epoch`): epoch at which to start propagation
+            initial_state (`np.array`): 6 dof initial state of spacecraft
+            TODO
 
         Attributes:
             propagator_config (`GVEPropagatorConfig`): propagator configuration
                 see `dm_propagator.py`
+            epoch (`Epoch`): current epoch
+            state (`np.array`): current state (assumed KOE TODO)
             TODO
         '''
 
         # Set attributes
         self.propagator_config = propagator_config
+        self.epoch = deepcopy(initial_epoch)
 
         # probs just its config data model and then propagation methods
         # themselves, aka step(), accelerations, etc
 
-    def step(self, timestep: float = None, to_epoch: Epoch = None):
+    def step(self, timestep: float = None):
+        '''Step forward in time
+
+        Args:
+            timestep (`float`): propagate state in time by this increment [s]
+
+        Modifies:
+            TODO
+
+        Returns:
+            TODO
+        '''
+        # If no timestep is provided, just step by default
+
+        # If timestep is provided, step by that amount
+
+        return
+
+    def step_to(self, timestep: float = None, to_epoch: Epoch = None):
         '''Step forward in time
 
         Args:
             timestep (`float`): propagate state in time by this increment [s]
                 or
-            to_epoch (`Epoch`): 
+            to_epoch (`Epoch`): epoch to propagate to
 
         Modifies:
             TODO
+
+        Returns:
+            TODO
+
+        Notes:
+            if no timestep or epoch is provided, step forward by default 
+                timestep (aka identical to step function)
         '''
         if timestep is not None:
-            print('hi')
+            # Step forward by a timestep
+
+            # Check that the timestep is not negative
+            if timestep < 0:
+                raise AbsoluteStateException(
+                    'Cannot propagate backwards. Quitting.')
+
+            return
 
         elif to_epoch is not None:
-            print('hey')
+            # Step forward to a given Epoch
+
+            # Check that the epoch is not in the past
+            if to_epoch - self.epoch < 0:
+                raise AbsoluteStateException(
+                    'Cannot propagate backwards. Quitting.')
+            return
+
+        else:
+            # No input provided, use default
+            # TODO call step
+            self.step()
+            return
 
         return
 
--- test/test_absolute_state.py
+++ test/test_absolute_state.py
@@ -10,5 +10,61 @@ import pytest
 from astrochelle.dynamics.absolute_state import *
 
 
-def test_nothing_here():
+def propagator_example()->GVEPropagator:
+    # Example propagator for use in tests
+    # Default configuration paramteres
+    timestep = 10  # 10 seconds
+    gravity_degree = 60
+    gravity_order = 60
+    flag_atmospheric_drag = True
+    model_atmospheric_drag = 'nrlmsise00'
+    flag_srp = True
+    model_srp = 'conical'
+    model_third_body = ['sun', 'moon']
+    flag_relativity = True
+
+    initial_epoch = Epoch(
+        year=2022,
+        month=8,
+        day=3,
+        hours=17,
+        minutes=19)
+
+    propagator_config = GVEPropagatorConfig(
+        timestep=timestep,
+        gravity_degree=gravity_degree,
+        gravity_order=gravity_order,
+        flag_atmospheric_drag=flag_atmospheric_drag,
+        model_atmospheric_drag=model_atmospheric_drag,
+        flag_solar_radiation_pressure=flag_srp,
+        model_solar_radiation_pressure=model_srp,
+        model_third_body=model_third_body,
+        flag_relativity=flag_relativity
+    )
+
+    propagator = GVEPropagator(
+        propagator_config=propagator_config,
+        initial_epoch=initial_epoch
+    )
+
+    return propagator
+
+
+def test_step():
+    # TODO check that it steps by the default timestep in config
+
+    # TODO
+
+
+def test_step_to_timestep():
+    # Raise error if timestep is negative
+
+    # TODO more
+    pass
+
+
+def test_step_to_to_epoch():
+    # Raise error if provided epoch is behind current epoch
+
+    # TODO more
     pass
