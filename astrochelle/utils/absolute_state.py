5a4d225b7dd225d2f21980cf8bc23bfcfcde811f
Michelle Chernick <mcparakeet@gmail.com>
2022-08-05 08:45:44 -0500

    framework for anomaly conversions

--- astrochelle/utils/absolute_state.py
+++ astrochelle/utils/absolute_state.py
@@ -45,21 +45,113 @@ def calculate_mean_motion(semimajor_axis: float) -> float:
     return sqrt(GM_EARTH / semimajor_axis**3)
 
 
-def construct_gve_matrix() -> np.ndarray:
+def convert_anomaly_mean_to_eccentric(semimajor_axis: float, eccentricity: float) -> float:
+    '''TODO
+
+    Args:
+        semimajor_axis (`float`): semi-major axis of the orbit [m]
+        eccentricity (`float`): eccentricity of the orbit
+
+    Returns:
+        TODO
+    '''
+    return
+
+
+def convert_anomaly_eccentric_to_true(semimajor_axis: float, eccentricity: float) -> float:
+    '''TODO
+
+    Args:
+        semimajor_axis (`float`): semi-major axis of the orbit [m]
+        eccentricity (`float`): eccentricity of the orbit
+
+    Returns:
+        TODO
+    '''
+    return
+
+
+def convert_anomaly_true_to_eccentric(semimajor_axis: float, eccentricity: float) -> float:
+    '''TODO
+
+    Args:
+        semimajor_axis (`float`): semi-major axis of the orbit [m]
+        eccentricity (`float`): eccentricity of the orbit
+
+    Returns:
+        TODO
+    '''
+    return
+
+
+def convert_anomaly_eccentric_to_mean(semimajor_axis: float, eccentricity: float) -> float:
+    '''TODO
+
+    Args:
+        semimajor_axis (`float`): semi-major axis of the orbit [m]
+        eccentricity (`float`): eccentricity of the orbit
+
+    Returns:
+        TODO
+    '''
+    return
+
+
+def convert_anomaly_mean_to_true(semimajor_axis: float, eccentricity: float) -> float:
+    '''TODO
+
+    Args:
+        semimajor_axis (`float`): semi-major axis of the orbit [m]
+        eccentricity (`float`): eccentricity of the orbit
+
+    Returns:
+        TODO
+    '''
+    return
+
+
+def convert_anomaly_true_to_mean(semimajor_axis: float, eccentricity: float) -> float:
+    '''TODO
+
+    Args:
+        semimajor_axis (`float`): semi-major axis of the orbit [m]
+        eccentricity (`float`): eccentricity of the orbit
+
+    Returns:
+        TODO
+    '''
+    return
+
+
+def construct_gve_matrix(koe: np.array) -> np.ndarray:
     '''Construct GVE matrix which represents the transition between
     accelerations in RTN to variations in the OE
 
     Args:
-        TODO
+        koe(`np.array`): Keplerian orbit elements[m, rad]
 
     Returns:
-        GVE matrix (6x3)
+        GVE matrix(6x3)
 
     Source:
         Ref. 1 page 567 (Eq. 8-24)
     '''
     gve_matrix = np.zeros((6, 3))
 
-    # TODO fill in here
+    # Extract OE
+    a = koe[0]
+    e = koe[1]
+    i = koe[2]
+    W = koe[3]
+    w = koe[4]
+    M = koe[5]
+
+    # Compute mean motion
+    mean_motion = calculate_mean_motion(semimajor_axis=a)
+
+    # Convert mean anomaly to true
+    nu = convert_anomaly_mean_to_true(semimajor_axis=a, eccentricity=e)
+
+    # TODO
 
     return gve_matrix
