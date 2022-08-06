dff16c24d190fb0f5e2b002ee8fc775256f632d6
Michelle Chernick <mcparakeet@gmail.com>
2022-08-06 08:28:45 -0400

    anudda unit test

--- test/test_absolute_state.py
+++ test/test_absolute_state.py
@@ -21,20 +21,47 @@ def test_convert_anomaly_mean_to_eccentric():
         mean_anomaly=mean_anomaly,
         eccentricity=eccentricity) - 3.8486617) < 1e-7
 
+    # Circular orbit, mean and eccentric should match
+    eccentricity = 1e-7
+    assert abs(convert_anomaly_mean_to_eccentric(
+        mean_anomaly=mean_anomaly,
+        eccentricity=eccentricity) - mean_anomaly) < 1e-7
+
 
 def test_convert_anomaly_eccentric_to_true():
     # TODO
     pass
+
+    # Circular orbit, eccentric and true should match
+    eccentric_anomaly = 1.123
+    eccentricity = 1e-7
+    assert abs(convert_anomaly_eccentric_to_true(
+        eccentric_anomaly=eccentric_anomaly,
+        eccentricity=eccentricity) - eccentric_anomaly) < 1e-7
 
 
 def test_convert_anomaly_true_to_eccentric():
     # TODO
     pass
 
+    # Circular orbit, eccentric and true should match
+    true_anomaly = 1.567
+    eccentricity = 1e-7
+    assert abs(convert_anomaly_true_to_eccentric(
+        true_anomaly=true_anomaly,
+        eccentricity=eccentricity) - true_anomaly) < 1e-7
+
 
 def test_convert_anomaly_eccentric_to_mean():
-    # TODO
-    pass
+    # From (reverse of) Example 4.1 on page 233 in Ref. 1
+    eccentric_anomaly = 3.8486617
+    eccentricity = 0.4
+
+    assert abs(convert_anomaly_eccentric_to_mean(
+        eccentric_anomaly=eccentric_anomaly,
+        eccentricity=eccentricity) - 235.4 * pi / 180) < 1e-7
+
+    # TODO more
 
 
 def test_convert_anomaly_mean_to_true():
